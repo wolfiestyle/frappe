5ed9d947b2acd8d32be4502e902da3c2ed6e9ee8
darkstalker <slayerbeast@gmail.com>
2019-01-05 21:06:03 -0300

    improved the async example

--- examples/async.rs
+++ examples/async.rs
@@ -1,4 +1,5 @@
 use frappe::Sink;
+use rand::distributions::Uniform;
 use rand::Rng;
 use std::thread;
 use std::time::Duration;
@@ -6,33 +7,36 @@ use std::time::Duration;
 fn main() {
     let sink = Sink::new();
 
+    // build the chain on the main thread
     let result = sink
         .stream()
         // we'll do this part on another thread
-        .map_n(move |arg, sender| {
+        .map_n(|arg, sender| {
             let n = *arg;
-            let main_th = thread::current();
             // our expensive computation (sleep sort)
             thread::spawn(move || {
                 thread::sleep(Duration::from_millis(n));
-                sender.send(n);
-                main_th.unpark(); // signal that there is data available
+                sender.send(n); // return the computed value
             });
         })
-        // the rest of the chain continues on the main thread as normal
-        .collect::<Vec<_>>();
+        // the rest of the stream chain is executed on the thread that called `Sender::send`
+        .fold(Vec::new(), |mut vec, n| {
+            vec.push(*n);
+            vec
+        });
 
     // now send some random values
     let mut rng = rand::thread_rng();
-    sink.feed((0..10).map(|_| rng.gen_range(0, 100)));
+    let input: Vec<_> = rng
+        .sample_iter(&Uniform::new_inclusive(0, 100))
+        .take(10)
+        .collect();
+    println!("input:  {:?}", input);
+    sink.feed(input);
+
+    // wait a bit for the threads to finish
+    thread::sleep(Duration::from_millis(150));
 
     // receive the results on the main thread
-    loop {
-        thread::park();
-        let res = result.sample();
-        println!("{:?}", res);
-        if res.len() >= 10 {
-            break;
-        }
-    }
+    println!("result: {:?}", result.sample());
 }
