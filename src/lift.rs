3e063e14297912405eb4ecf788cb7c07d98b5f10
darkstalker <slayerbeast@gmail.com>
2019-01-06 21:32:35 -0300

    rustfmt seems to ignore this file

--- src/lift.rs
+++ src/lift.rs
@@ -9,54 +9,59 @@ use crate::types::{SharedImpl, SharedSignal, Storage};
 /// into a `Signal<R>` that computes it's value by sampling the input signals and then
 /// calling the supplied function.
 #[macro_export]
-macro_rules! signal_lift
-{
-    ($($sig:expr),+ => $f:expr) => ( signal_lift!($f, $($sig),+) );
+macro_rules! signal_lift {
+    ($($sig:expr),+ => $f:expr) => {
+        signal_lift!($f, $($sig),+)
+    };
 
-    ($f:expr) => ($crate::Signal::from_fn($f));
+    ($f:expr) => {
+        $crate::Signal::from_fn($f)
+    };
 
-    ($f:expr, $sig1:expr) => ( $crate::Signal::map(&$sig1, $f) );
+    ($f:expr, $sig1:expr) => {
+        $crate::Signal::map(&$sig1, $f)
+    };
 
-    ($f:expr, $sig1:expr, $sig2:expr) =>
-        ($crate::lift::lift2($f, $sig1, $sig2));
+    ($f:expr, $sig1:expr, $sig2:expr) => {
+        $crate::lift::lift2($f, $sig1, $sig2)
+    };
 
-    ($f:expr, $sig1:expr, $sig2:expr, $sig3:expr) =>
-        ($crate::lift::lift3($f, $sig1, $sig2, $sig3));
+    ($f:expr, $sig1:expr, $sig2:expr, $sig3:expr) => {
+        $crate::lift::lift3($f, $sig1, $sig2, $sig3)
+    };
 
-    ($f:expr, $sig1:expr, $sig2:expr, $sig3:expr, $sig4:expr) =>
-        ($crate::lift::lift4($f, $sig1, $sig2, $sig3, $sig4));
+    ($f:expr, $sig1:expr, $sig2:expr, $sig3:expr, $sig4:expr) => {
+        $crate::lift::lift4($f, $sig1, $sig2, $sig3, $sig4)
+    };
 
-    ($f:expr, $sig1:expr, $sig2:expr, $sig3:expr, $sig4:expr, $sig5:expr) =>
-        ($crate::lift::lift5($f, $sig1, $sig2, $sig3, $sig4, $sig5));
+    ($f:expr, $sig1:expr, $sig2:expr, $sig3:expr, $sig4:expr, $sig5:expr) => {
+        $crate::lift::lift5($f, $sig1, $sig2, $sig3, $sig4, $sig5)
+    };
 
-    ($f:expr, $sig1:expr, $sig2:expr, $sig3:expr, $sig4:expr, $sig5:expr, $sig6:expr) =>
-        ($crate::lift::lift6($f, $sig1, $sig2, $sig3, $sig4, $sig5, $sig6));
+    ($f:expr, $sig1:expr, $sig2:expr, $sig3:expr, $sig4:expr, $sig5:expr, $sig6:expr) => {
+        $crate::lift::lift6($f, $sig1, $sig2, $sig3, $sig4, $sig5, $sig6)
+    };
 }
 
-macro_rules! lift_impl
-{
+macro_rules! lift_impl {
     ($fname:ident ( $($vname:ident : $tname:ident),+ ) $($idx:tt)+) => (
         impl<T, $($tname,)+ F> SharedSignal<T> for SharedImpl<T, ($(Signal<$tname>),+), F>
-            where F: Fn($($tname),+) -> T + 'static,
+        where
+            F: Fn($($tname),+) -> T + 'static,
             $($tname: Clone + 'static),+
         {
-            fn update(&self)
-            {
-                if !self.storage.must_update() && ($(self.source.$idx.has_changed())||+)
-                {
+            fn update(&self) {
+                if !self.storage.must_update() && ($(self.source.$idx.has_changed())||+) {
                     self.storage.inc_root();
                 }
             }
 
-            fn get_storage(&self) -> &Storage<T>
-            {
+            fn get_storage(&self) -> &Storage<T> {
                 &self.storage
             }
 
-            fn sample(&self) -> &Storage<T>
-            {
-                if self.storage.must_update()
-                {
+            fn sample(&self) -> &Storage<T> {
+                if self.storage.must_update() {
                     let val = (self.f)($(self.source.$idx.sample()),+);
                     self.storage.set_local(val);
                 }
@@ -66,7 +71,8 @@ macro_rules! lift_impl
 
         /// Lifts a function into a signal.
         pub fn $fname<T, F, $($tname),+>(f: F, $($vname: Signal<$tname>),+) -> Signal<T>
-            where F: Fn($($tname),+) -> T + Send + Sync + 'static,
+        where
+            F: Fn($($tname),+) -> T + Send + Sync + 'static,
             T: Send + 'static, $($tname: Clone + Send + Sync + 'static),+
         {
             Signal::shared(SharedImpl{
