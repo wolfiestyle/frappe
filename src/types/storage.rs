3c33246f62c39e65443cd207d60af8c69ee70777
darkstalker <slayerbeast@gmail.com>
2019-01-11 13:28:30 -0300

    not using serials anymore

--- src/types/storage.rs
+++ src/types/storage.rs
@@ -1,10 +1,8 @@
 use parking_lot::Mutex;
-use std::sync::atomic::{AtomicUsize, Ordering};
 
 /// Storage cell for shared signal values.
 pub struct Storage<T> {
     val: Mutex<Option<T>>,
-    serial: AtomicUsize,
 }
 
 const ERR_EMPTY: &str = "storage empty";
@@ -14,7 +12,6 @@ impl<T> Storage<T> {
     pub fn new(val: T) -> Self {
         Storage {
             val: Mutex::new(Some(val)),
-            serial: AtomicUsize::new(1),
         }
     }
 
@@ -26,17 +23,14 @@ impl<T> Storage<T> {
         self.val.lock().clone().expect(ERR_EMPTY)
     }
 
-    /// Sets value and increments the serial.
+    /// Sets the value.
     pub fn set(&self, val: T) {
         *self.val.lock() = Some(val);
-        self.inc_serial();
     }
 
     /// Replaces the stored value and returns the previous one.
     pub fn replace(&self, val: T) -> T {
-        let old = self.val.lock().replace(val);
-        self.inc_serial();
-        old.expect(ERR_EMPTY)
+        self.val.lock().replace(val).expect(ERR_EMPTY)
     }
 
     /// Passes the stored value through a function.
@@ -47,13 +41,6 @@ impl<T> Storage<T> {
         let mut st = self.val.lock();
         let old = st.take().expect(ERR_EMPTY);
         *st = Some(f(old));
-        drop(st);
-        self.inc_serial();
-    }
-
-    /// Increments the serial of this storage.
-    fn inc_serial(&self) {
-        self.serial.fetch_add(1, Ordering::Release);
     }
 }
 
@@ -62,7 +49,6 @@ impl<T> Default for Storage<T> {
     fn default() -> Self {
         Storage {
             val: Default::default(),
-            serial: Default::default(),
         }
     }
 }
