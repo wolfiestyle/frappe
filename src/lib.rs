b54140ac2e4afbdb74dbfbe2cd32a68015b4819f
darkstalker <slayerbeast@gmail.com>
2019-02-14 18:45:59 -0300

    removed unneeded arbitrary_self_types feature

--- src/lib.rs
+++ src/lib.rs
@@ -5,10 +5,7 @@
 //!
 //! See each module documentation for more details.
 #![warn(missing_docs)]
-#![cfg_attr(
-    feature = "nightly",
-    feature(arbitrary_self_types, futures_api, async_await, await_macro)
-)]
+#![cfg_attr(feature = "nightly", feature(futures_api, async_await, await_macro))]
 
 #[macro_use]
 mod helpers;
